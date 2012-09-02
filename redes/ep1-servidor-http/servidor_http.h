6fb129cde8b35dba1b0c0d4e84e36b93da18c126
Pedro Paulo <pedropaulovc@gmail.com>
2012-09-02 20:55:55 -0300

    Redes: Código documentado

--- redes/ep1-servidor-http/servidor_http.h
+++ redes/ep1-servidor-http/servidor_http.h
@@ -116,71 +116,74 @@ requisicao interpretar_requisicao(char requisicao[]);
 /**
  * Recebe como parâmetro uma struct do tipo resposta devidamente populada 
  * e o tipo da requisição pedida pelo cliente. Com estes dados envia todos os
- * cabeçalhos relevantes ao cliente.
- *
+ * cabeçalhos relevantes ao cliente, tais como: resultado da requisição (200 OK),
+ * Date, Server, Last-Modificed, Etag, Accept-Ranges, 
+ * Content-Length, Vary, Connection e Content-Type. Termina o envio de cabeçalhos
+ * com o envio de uma quebra de linha (\r\n) extra.
  */
 void enviar_resposta(resposta resp, tipo_requisicao tipo);
 /**
- *
- *
- *
- *
+ * Recebe o caminho para o arquivo a ser enviado após os cabeçalhos. Divide a
+ * operação em blocos de MAXLINE bytes.
  */
 void enviar_arquivo(char nome_arquivo[]);
 /**
- *
- *
- *
- *
+ * Recebe como parâmetro o nome de um arquivo que se deseja obter o Content-Type
+ * correspondente retorna uma string estaticamente alocada contendo o valor
+ * correspondente. Aceita arquivos .htm(l), .jp(e)g, .gif, .txt, .pdf. Para outros
+ * arquivos retorna application/octet-stream.
  */
 char* obter_content_type(char nome_arquivo[]);
 /**
- *
- *
- *
- *
+ * Recebe como parâmetro um caminho para o diretório de onde serão servidos os
+ * arquivos pelo servidor web.
  */
 void definir_diretorio_www(char diretorio[]);
 /**
- *
- *
- *
- *
+ * Recebe como parâmetrox uma string "string" de tamanho "tam_string" e armazena
+ * no buffer do servidor. Caso o buffer não tenha capacidade vaga o suficiente,
+ * envia seu conteúdo atual e o substitui pelo conteúdo de "string". O 
+ * comportamento para valores de "tam_string" maiores que MAXLINE é
+ * indeterminado.
  */
 void escrever_buffer(char string[], int tam_string);
 /**
- *
- *
- *
- *
+ * Envia os conteúdos atualmente contidos no buffer para o cliente e marca o 
+ * buffer como vazio novamente.
  */
 void enviar_buffer();
 /**
- *
- *
- *
- *
+ * Função principal do servidor web. Recebe como parâmetro o descritor do socket
+ * utilizado pelo programa para comunicação com o cliente. Com o auxílio das
+ * funções descritas neste módulo é responsável por ler a requisição do cliente,
+ * interpretar o pedido, e enviar a resposta adequada. Aceita requisições GET e
+ * POST. No primeiro caso, envia o arquivo requisitado caso exista, o index.html
+ * caso seja requisitado um diretório ou uma página de erro 404.html caso o 
+ * arquivo não exista. No segundo caso, envia como resposta os campos lidos do
+ * usuário.
+ * 
+ * Obs: A página 404.html deve residir no diretório www (O de execução do 
+ * programa, por default. 
  */
 void executar_servidor_http(int socket);
 /**
- *
- *
- *
- *
+ * Função responsável por ler uma linha terminada com \r\n ou \n do socket e 
+ * escrever na string passada por parâmetro linha. O tamanho máximo da string
+ * é definida no parâmetro tam_max. linha ao final da execução é uma string 
+ * terminada com \0 e sem os caracteres de quebra de linha.
  */
-int ler_linha(char linha[], int tam);
+int ler_linha(char linha[], int tam_max);
 /**
- *
- *
- *
- *
+ * Dada uma linha contendo o conteúdo da requisição POST vinda do cliente
+ * (Ex. "campo1=valor1&campo2=valor2") faz o parsing desta string e popula as
+ * variáveis qtd_campos_post, chaves_post e valores_post com os valores lidos
+ * desta string.
  */
 void interpretar_post(char linha[]);
 /**
- *
- *
- *
- *
+ * Envia de volta ao usuário os conteúdos armazenados nas variáveis 
+ * qtd_campos_post, chaves_post e valores_post em um formato tabular em texto 
+ * puro.
  */
 void enviar_conteudo_post();
 
