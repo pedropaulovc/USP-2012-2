43c4decc75d116ecdb546245e33fef010946c8a5
Pedro Paulo <pedropaulovc@gmail.com>
2012-12-07 13:09:25 -0200

    300: Implementado Golub-Reinsch

--- 300/ep3-compressao-img/ep3pedro.m
+++ 300/ep3-compressao-img/ep3pedro.m
@@ -7,65 +7,208 @@
 
 1;
 
-function [U, A, V] = golub_kahan_bidiagonalization(A)
-	[m,n] = size(A);
-	
-	if m < n
-		tmp = m;
-		m = n;
-		n = tmp;
-		A = A';
+function [res] = sinal(a,b)
+	if b >= 0.0
+		res = abs(a);
+	else
+		res = -abs(a);
 	endif
-	
-	U = eye(m,m);
-	V = eye(n,n);
-	
-	for k = 1:n
-		x = A(k:m,k);
-		x(1) = x(1) + sign(x(1)) * norm(x);
-		u = x/norm(x);
-		A(k:m,k:n) = A(k:m,k:n) - 2*u*( u'*A(k:m,k:n) );
-		
-		u_tmp = [zeros(m - rows(u),1);u];
-		Qk = eye(m, m) - 2 * u_tmp * u_tmp';
-		U = U * Qk;
-		
-		if k <= n-2
-			x = A(k,k+1:n);
-			x(1) = x(1) + sign(x(1)) * norm(x);
-			v = x/norm(x);
-			A(k:m,k+1:n) = A(k:m,k+1:n) - 2*( A(k:m,k+1:n)*v' )*v;
-			
-			v_tmp = [zeros(n - columns(v)),v];
-			Pk1 = eye(n, n) - 2*v_tmp'*v_tmp;
-			V = Pk1 * V;
-		endif 
-	endfor
 endfunction
 
-function [U, S, V] = golub_reinsch_svd(A)
-	[m, n] = size(A)
-	epsilon = erro()
-	[U, B, V] = golub_kahan_bidiagonalization(A)
+function [a, w, v] = svdcmp(a)
+	[m, n] = size(a);
 	
-	while true
-		for i = 1 : n - 1
-			if abs(B(i, i + 1) <= epsilon * (abs(B(i,i)) + abs(B(i + 1, i + 1)))
-				B(i, i + 1) = 0
+	rv1 = zeros(n,1);
+	w = zeros(n,1);
+	v = zeros(n);
+	
+	g = scale = anorm = 0.0;
+	% Householder reduction to bidiagonal form
+	for i = 1:n
+		l = i + 1;
+		rv1(i) = scale * g;
+		g = s = scale = 0.0;
+		if (i <= m)
+			for k = i:m	scale += abs(a(k,i)); endfor
+			if scale
+				for k = i:m
+					a(k,i) /= scale;
+					s += a(k,i) * a(k,i);
+				endfor
+				
+				f = a(i,i);
+				g = -sinal(sqrt(s),f);
+				h = f * g - s;
+				a(i,i) = f - g;
+				for j = l:n
+					s = 0.0;
+					for k = i:m s += a(k,i) * a(k,j); endfor
+					f = s/h;
+					for k = i:m a(k,j) += f * a(k,i); endfor
+				endfor
+				for k = i:m a(k,i) *= scale; endfor
 			endif
-			
-			
+		endif
+		
+		w(i) = scale * g;
+		g = s = scale = 0.0;
+		
+		if i <= m && i != n
+			for k = l:n scale += abs(a(i,k)); endfor
+			if scale
+				for k = l:n
+					a(i,k) /= scale;
+					s += a(i,k) * a(i,k);
+				endfor
+				f = a(i,l);
+				g = -sinal(sqrt(s),f);
+				h = f * g - s;
+				a(i,l) = f - g;
+				for k = l:n rv1(k) = a(i,k)/h; endfor
+				for j = l:m
+					s = 0.0;
+					for k = l:n s += a(j,k) * a(i,k); endfor
+					for k = l:n a(j,k) += s * rv1(k); endfor
+				endfor
+				for k = l:n a(i,k) *= scale; endfor
+			endif
+		endif
+		anorm = max(anorm, (abs(w(i)) + abs(rv1(i))));
+	endfor
+	% Accumulation of right-hand transformations.
+	for i = n:-1:1
+		if i < n
+			if g
+				for j = l:n %Double division to avoid possible underflow.
+					v(j,i) = (a(i,j)/a(i,l))/g;
+				endfor
+				for j = l:n
+					s = 0.0;
+					for k = l:n s += a(i,k) * v(k,j); endfor
+					for k = l:n v(k,j) += s * v(k,i); endfor
+				endfor
+			endif
+			for j = l:n v(i,j) = v(j,i) = 0.0; endfor
+		endif
+		v(i,i) = 1.0;
+		g = rv1(i);
+		l = i;
+	endfor
+	% Accumulation of left-hand transformations.
+	for i = min(m,n):-1:1
+		l = i + 1;
+		g = w(i);
+		for j = l:n a(i,j) = 0.0; endfor
+		if g
+			g = 1.0/g;
+			for j = l:n
+				s = 0.0;
+				for k = l:m s += a(k,i) * a(k,j); endfor
+				f = (s/a(i,i)) * g;
+				for k = i:m a(k,j) += f * a(k,i); endfor
+			endfor
+			for j = i:m a(j,i) *= g; endfor
+		else
+			for j = i:m a(j,i) = 0.0; endfor
+		endif
+		a(i,i) += 1;
+	endfor
+	% Diagonalization of the bidiagonal form.
+	for k = n:-1:1
+		for its = 1:30
+			flag = 1;
+			% Test for splitting.
+			for l = k:-1:1
+				nm = l - 1; % Note that rv1(1) is always zero.
+				if abs(rv1(l)) + anorm == anorm
+					flag = 0;
+					break;
+				endif
+				if abs(w(nm)) + anorm == anorm break; endif
+			endfor
+			if flag
+				c = 0.0; % Cancellation of rv1(l), if l > 1.
+				s = 1.0;
+				for i = l:k
+					f = s * rv1(i);
+					rv1(i) = c * rv1(i);
+					if abs(f) + anorm == anorm break; endif
+					g = w(i);
+					h = hypot(f,g);
+					w(i) = h;
+					h = 1.0/h;
+					c = g * h;
+					s = -f * h;
+					for j = 1:m
+						y = a(j,nm);
+						z = a(j,i);
+						a(j,nm) = y * c + z * s;
+						a(j,i) = z * c - y * s;
+					endfor
+				endfor
+			endif
+			z = w(k);
+			% Convergence.
+			if l == k
+				% Singular value is made nonnegative.
+				if z < 0.0
+					w(k) = -z;
+					for j = 1:n v(j,k) = -v(j,k); endfor
+				endif
+				break;
+			endif
+			if its == 30 printf("no convergence in 30 svdcmp iterations"); endif
+			x = w(l); % Shift from bottom 2-by-2 minor.
+			nm = k - 1;
+			y = w(nm);
+			g = rv1(nm);
+			h = rv1(k);
+			f = ((y-z)*(y+z)+(g-h)*(g+h))/(2.0*h*y);
+			g = hypot(f,1.0);
+			f=((x-z)*(x+z)+h*((y/(f+sinal(g,f)))-h))/x;
+			c = s = 1.0;
+			% Next QR transformation:
+			for j = l:nm
+				i = j + 1;
+				g = rv1(i);
+				y = w(i);
+				h = s * g;
+				g = c * g;
+				z = hypot(f, h);
+				rv1(j) = z;
+				c = f / z;
+				s = h / z;
+				f = x * c + g * s;
+				g = g * c - x * s;
+				h = y * s;
+				y *= c;
+				for jj = 1:n
+					x = v(jj,j);
+					z = v(jj,i);
+					v(jj,j) = x * c + z * s;
+					v(jj,i) = z * c - x * s;
+				endfor
+				z = hypot(f,h);
+				w(j) = z; % Rotation can be arbitrary if z = 0.
+				if z
+					z = 1.0 / z;
+					c = f * z;
+					s = h * z;
+				endif
+				f = c * g + s * y;
+				x = c * y - s * g;
+				for jj = 1:m
+					y=a(jj,j);
+					z=a(jj,i);
+					a(jj,j) = y * c + z * s;
+					a(jj,i) = z * c - y * s;
+				endfor
+			endfor
+			rv1(l) = 0.0;
+			rv1(k) = f;
+			w(k) = x;
 		endfor
-	endwhile
-
-endfunction
-
-function [B, Q, P] = golub_kahan_svd_step(A)
-
-endfunction
-
-function [e] = erro()
-	e = sqrt(eps)
+	endfor	
 endfunction
 
 if(nargin < 3)
